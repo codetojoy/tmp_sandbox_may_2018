abb1424213bd20270f3499db8dcf01b60f1101e6
Michael Easter <codetojoy@gmail.com>
2018-05-14 10:14:16 -0300

    update for issue 5150

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,7 +2,7 @@
 package com.example;
 
 public class App {
-    private static final String VERSION_INFO = "V 1.0.0 master";
+    private static final String VERSION_INFO = "V 1.0.0";
 
     public static void main(String[] args) {
         System.out.println("TRACER hello from app. version: " + VERSION_INFO);
---
+++ src/main/java/com/example/Service.java
@@ -0,0 +1,8 @@
+
+package com.example;
+
+public class Service {
+    public String getServiceName() {
+        return "example service name";
+    }
+}
